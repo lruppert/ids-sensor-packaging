8406d7e9d5da9608d6bcdbdee00737e400893ed7
Lou Ruppert <himself@louruppert.com>
2014-09-11 18:40:34 +0000

    Bugfix 2.3.1

--- config.mk
+++ config.mk
@@ -11,7 +11,7 @@ PF_RING_DL_URL := $(PF_RING_SCHEME)://$(PF_RING_HOST)$(PF_RING_PATH)/$(PF_RING_T
 #
 # bro sources
 #
-BRO_VERSION ?= 2.3
+BRO_VERSION ?= 2.3.1
 BRO_SCHEME := http
 BRO_HOST := www.bro.org
 BRO_PATH := /downloads/release
