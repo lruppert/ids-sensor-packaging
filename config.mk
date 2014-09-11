3dda49975c17c6b409f2ea03c3770a5a8f3696e1
Lou Ruppert <himself@louruppert.com>
2014-09-11 19:03:10 +0000

    Bro uses https now

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ PF_RING_DL_URL := $(PF_RING_SCHEME)://$(PF_RING_HOST)$(PF_RING_PATH)/$(PF_RING_T
 # bro sources
 #
 BRO_VERSION ?= 2.3.1
-BRO_SCHEME := http
+BRO_SCHEME := https
 BRO_HOST := www.bro.org
 BRO_PATH := /downloads/release
 BRO_TARBALL := bro-$(BRO_VERSION).tar.gz
